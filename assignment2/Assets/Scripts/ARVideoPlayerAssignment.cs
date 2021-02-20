eee9deb0ebdb8bb03591d7fb6191c30d7b32b12e
Ju Xian Giam <giamjuxian@gmail.com>
2021-02-20 15:48:44 +0800

    Remove commented script

--- assignment2/Assets/Scripts/ARVideoPlayerAssignment.cs
+++ assignment2/Assets/Scripts/ARVideoPlayerAssignment.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -47,38 +47,22 @@ public class ARVideoPlayerAssignment : MonoBehaviour
                 .SetActive(trackedImage.trackingState != TrackingState.None &&
                 trackedImage.trackingState != TrackingState.Limited);
 
-            if (trackedImage.trackingState == TrackingState.None ||
-                trackedImage.trackingState == TrackingState.Limited)
+            if (
+                trackedImage.trackingState == TrackingState.None ||
+                trackedImage.trackingState == TrackingState.Limited
+            )
             {
                 currentImageText.text = "<b>Scan image</b>";
             }
-            // TODO: Keep this in case we wanna switch back, remove before submission
-            // VideoPlayer videoPlayer =
-            //     model.GetComponentInChildren<VideoPlayer>();
-
-            // if (videoPlayer)
-            // {
-            //     switch (imageName)
-            //     {
-            //         case "Synergy":
-            //             videoPlayer.clip = videoSynergy;
-            //             Debug.Log("should play synergy");
-            //             break;
-            //         case "EML":
-            //             videoPlayer.clip = videoEML;
-            //             Debug.Log("should play EML");
-            //             break;
-            //         case "GENUS":
-            //             videoPlayer.clip = videoGENUS;
-            //             Debug.Log("should play GENUS");
-            //             break;
-            //     }
-            //     // videoPlayer.Play();
-            // }
         }
         foreach (var trackedImage in args.added)
         {
-            VideoPlayer videoPlayer = trackedImage.transform.GetChild(0).GetComponentInChildren<VideoPlayer>();
+            VideoPlayer videoPlayer =
+                trackedImage
+                    .transform
+                    .GetChild(0)
+                    .GetComponentInChildren<VideoPlayer>();
+
             string imageName = trackedImage.referenceImage.name;
             currentImageText.text = "<b>Now playing:" + imageName + "</b>";
 
@@ -88,15 +72,12 @@ public class ARVideoPlayerAssignment : MonoBehaviour
                 {
                     case "Synergy":
                         videoPlayer.clip = videoSynergy;
-                        Debug.Log("should play synergy");
                         break;
                     case "EML":
                         videoPlayer.clip = videoEML;
-                        Debug.Log("should play EML");
                         break;
                     case "GENUS":
                         videoPlayer.clip = videoGENUS;
-                        Debug.Log("should play GENUS");
                         break;
                 }
             }
