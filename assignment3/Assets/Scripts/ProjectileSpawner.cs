65374eb3090027a6303d89f70772abe76a88f576
lws803 <lws803@gmail.com>
2021-02-28 23:01:41 +0800

    added repeating spawner

---
+++ assignment3/Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float interval;
+    public float spawnRadius;
+    public float heightOffset;
+    public int maxProjectiles;
+    public bool isFixedPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnProjectile", 0.0f, interval);
+    }
+
+    private void SpawnProjectile()
+    {
+        if (isFixedPoint)
+        {
+            if (this.transform.childCount < 1)
+            {
+                Instantiate(
+                    projectilePrefab,
+                    new Vector3(0, heightOffset, 0), Quaternion.identity,
+                    transform
+                );
+            }
+        } else {
+            if (this.transform.childCount < maxProjectiles)
+            {
+                Vector2 randPos = Random.insideUnitCircle * spawnRadius;
+                Instantiate(
+                    projectilePrefab,
+                    new Vector3(randPos.x, heightOffset, randPos.y),
+                    Quaternion.identity,
+                    transform
+                );
+            }
+        }
+    }
+}
