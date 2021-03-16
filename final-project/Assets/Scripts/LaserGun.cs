d93c83dbb23be8932c0c3e35936388f5e59d418c
Marcus Phua Zheng Jie <32394182+marcus-pzj@users.noreply.github.com>
2021-03-16 17:41:00 +0800

    [VRK-30] Light physics (#16)
    * Asset folder structure and Reflection
    
    * Bug fixes
    
    * Refraction - One Layer
    
    * Added some material and light assets
    
    * Polished Refraction properties in a solid object
    
    * Added Mirror property and Medium Refractive Index
    
    * Remove Light Source Collider
    
    * converted blend file to fbx

---
+++ final-project/Assets/Scripts/LaserGun.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        LaserManager.instance.AddLaser(this);
+    }
+}
