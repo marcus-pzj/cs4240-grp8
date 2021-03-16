d8e90622a173ce5ff8420975b5e416653e63fc46
lws803 <lws803@gmail.com>
2021-03-16 21:53:05 +0800

    fix weird bug with reference image failed to load resouce

--- final-project/Assets/Scripts/TrackedPrefabManager.cs
+++ final-project/Assets/Scripts/TrackedPrefabManager.cs
@@ -31,23 +31,34 @@ public class TrackedPrefabManager : MonoBehaviour
         foreach (var trackedImage in e.added)
         {
             // Prefab here must have already been preloaded before hand
-            trackedImage.transform.GetChild(0).gameObject.SetActive(true);
+            UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, false);
         }
 
         foreach (var trackedImage in e.updated)
         {
             if (trackedImage.trackingState != TrackingState.Tracking)
             {
-                trackedImage.transform.GetChild(0).gameObject.SetActive(false);
+                UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, false);
             } else
             {
-                trackedImage.transform.GetChild(0).gameObject.SetActive(true);
+                UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, true);
             }
 
         }
         foreach (var trackedImage in e.removed)
         {
-            trackedImage.transform.GetChild(0).gameObject.SetActive(false);
+            UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, false);
+        }
+    }
+
+    private void UpdatePrefab(string label, Transform trackedImageTransform, bool active)
+    {
+        if (label == "glass")
+        {
+            trackedImageTransform.GetChild(1).gameObject.SetActive(active);
+        } else
+        {
+            trackedImageTransform.GetChild(0).gameObject.SetActive(active);
         }
     }
 }
