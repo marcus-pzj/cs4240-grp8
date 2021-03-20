9a8c6e61c6e1b979e3ee8d4e6e0af83557f884ae
Wilson Ler <lws803@gmail.com>
2021-03-20 13:50:24 +0800

    [VRK-29] Multiple card tracking (#15)
    * Added AR kit stuff for project genesis
    
    * added sample AR scene
    
    * modified sample AR scene
    
    * added card scene
    
    * card scene with trackable image
    
    * added tracked prefab manager script
    
    * added some todos
    
    * modified script
    
    * added todo
    
    * fixing multiple trackable objects script
    
    * succesfully loaded and unloaded prefabs
    
    * fix weird bug with reference image failed to load resouce
    
    * working multiple tracking
    
    * cleanup
    
    * added laser pointer prefab
    
    * working laser puck and glass prefab
    
    * adjusted laser girth
    
    * destroy all lasers when inactive
    
    * scaled up ARSessionOrigin
    
    * working laser and prefabs

---
+++ final-project/Assets/Scripts/TrackedPrefabManager.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedPrefabManager : MonoBehaviour
+{
+    private ARTrackedImageManager manager;
+    private GameObject laserManager;
+
+    public GameObject laserPointer;
+    public GameObject glass;
+
+    private void Start()
+    {
+        laserPointer = Instantiate(laserPointer, Vector3.zero, Quaternion.identity);
+        glass = Instantiate(glass, Vector3.zero, Quaternion.identity);
+        laserPointer.SetActive(false);
+        glass.SetActive(false);
+        laserManager = GameObject.FindGameObjectWithTag("LaserManager");
+        laserManager.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        manager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        manager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        manager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs e)
+    {
+
+        foreach (var trackedImage in e.added)
+        {
+            UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, true);
+        }
+
+        foreach (var trackedImage in e.updated)
+        {
+            if (trackedImage.trackingState != TrackingState.Tracking)
+            {
+                UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, false);
+            }
+            else
+            {
+                UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, true);
+            }
+
+        }
+        foreach (var trackedImage in e.removed)
+        {
+            UpdatePrefab(trackedImage.referenceImage.name, trackedImage.transform, false);
+        }
+    }
+
+    private void UpdatePrefab(string label, Transform trackedImageTransform, bool active)
+    {
+        // Use laser as an "anchor" -> but how do we find the transform and rotation for anchor?
+        if (label == "glass")
+        {
+            glass.SetActive(active);
+            glass.transform.SetPositionAndRotation(
+                trackedImageTransform.position,
+                //Quaternion.Euler(0, trackedImageTransform.rotation.y, 0)
+                trackedImageTransform.rotation
+            );
+        }
+        else
+        {
+            handleLaserManager(active);
+            laserPointer.SetActive(active);
+            laserPointer.transform.SetPositionAndRotation(
+                //new Vector3(trackedImageTransform.position.x, glass.transform.position.y, trackedImageTransform.position.z),
+                trackedImageTransform.position,
+                //Quaternion.Euler(0, trackedImageTransform.rotation.y, 0)
+                trackedImageTransform.rotation
+            );
+            // TODO: We might have to average out the rotation and position
+        }
+    }
+
+    private void handleLaserManager(bool active)
+    {
+        laserManager.SetActive(active);
+        if (!active)
+        {
+            laserManager.GetComponent<LaserManager>().DestroyAllLasers();
+        }
+    }
+}
