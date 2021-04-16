5fda4c7f3825e74616f705cedb061677260a4e96
marcus_pzj <marcuszhengjie@gmail.com>
2021-04-16 16:05:17 +0800

    deleted previous assignments

--- assignment2/Packages/com.kyrylokuzyk.arfoundationremote/Runtime/Constants.cs
+++
@@ -1,8 +0,0 @@
-
-
-namespace ARFoundationRemote.Runtime {
-    public static class Constants {
-        public const string packageName = "AR Foundation Remote";
-        public const string disableStartupErrorsMessage = "To silent this error, please disable the 'Settings/Debug Settings/Log Startup Errors'.";
-    }
-}
